31b055651ced5286897d977e6c2331778b4ab188
Adam <asantillana@apple.com>
2022-11-19 15:39:34 -0600

    Added code to display current date at top of calendar.

--- Develop/script.js
+++ Develop/script.js
@@ -15,9 +15,16 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  currentHour = dayjs().hour();
+  console.log(currentHour);
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // DONE: Add code to display the current date in the header of the page.
+  // Desired format: Monday, December 13th
+  var now = dayjs();
+  $('#currentDay').text(now.format("dddd, MMMM D"));
 });
